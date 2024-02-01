9082c9de728adc15878eecca8bc1c7373d54b9cc
Vishnu CP <vishnucpmcd@gmail.com>
2024-02-01 13:39:11 +0530

    error page edited

--- src/components/Error/Error404.jsx
+++ src/components/Error/Error404.jsx
@@ -8,6 +8,7 @@ function Error404() {
         <div className='content'>
             <h1 className='error'>Error 404: Not Found</h1>
             <p>The page you are looking for does not available at the moment.</p>
+            <h3>you must login to view this page</h3>
             <small><Link to={'/'}>click here</Link> to goto Hompage</small>
         </div>
     )
